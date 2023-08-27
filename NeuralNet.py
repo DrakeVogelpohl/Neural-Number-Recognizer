71766cef3eb827902fca7be1e4d3d961089ddb3b
DrakeVogelpohl <drakevogelpohl2026@u.northwestern.edu>
2023-08-27 11:27:13 -0600

    Training and Testing

--- NeuralNet.py
+++ NeuralNet.py
@@ -59,9 +59,12 @@ class NeuralNet:
     # Back Propagation
     # The math was done on a separate doc linked in the ReadMe
     def __back_prop(self, y, a, z, w, numHiddenL, trainSize):
+        ## MIGHT HAVE TO NORMALIZE AT END ##
+        # Need to check the math first + test both with the data and see which one is better
+
         # Last layer
         dCdbL = 2*(a[numHiddenL + 1] - y) * self.__SoftmaxDerivative(z[numHiddenL])
-        db = [((1/trainSize) * np.sum(dCdbL, axis=1))] # MIGHT HAVE TO NORMALIZE AT END
+        db = [((1/trainSize) * np.sum(dCdbL, axis=1))]
 
         dCdwL = db[0].dot(a[numHiddenL].T)
         dw = [((1/trainSize) * np.sum(dCdwL, axis=1))]
@@ -74,7 +77,17 @@ class NeuralNet:
             dCdw = db[n+1].dot(a[numHiddenL-1 -n].T)
             dw.append(((1/trainSize) * np.sum(dCdw, axis=1)))      
 
-        return dw.reverse(), db.reverse()
+        # Reverse order
+        dw.reverse()
+        db.reverse()
+        return dw, db
+    
+
+    # Update weights and biases
+    def __update_wb(w, b, dw, db, alpha):
+        new_w = w - dw * alpha
+        new_b = b - db * alpha
+        return new_w, new_b
 
 
     # Turns Y_train into usable data for our net. Does this by turing a number
@@ -84,9 +97,38 @@ class NeuralNet:
         y = np.zeros(trainSize, outputLS)
         y[np.arange(trainSize), Y] = 1
         return y.T
+    
+    # Get the predictions from the net
+    def __get_predictions(a, numHiddenL):
+        lastLayerOutput = a[numHiddenL + 1]
+        return np.argmax(lastLayerOutput, 0)
+    
+    # Get the accuracy of the net compared to the data
+    def __get_accuracy(predictions, Y, Y_size):
+        return np.sum(predictions == Y) / Y_size
+
+
 
     # Public method that trains the net with the given data with the given number
     # of iterations and the learning rate alpha 
-    def train(self, A0, Y_train, iterations, alpha):
-        pass
+    def train(self, A0_train, Y_train, trainSize, iterations, alpha):
+        y = self.__makeYUsable(Y_train)
+
+        for i in range(iterations):
+            a, z = self.__forward_prop(A0_train, self.w, self.b, self.numHiddenL)
+            dw, db = self.__back_prop(y, a, z, self.w, self.numHiddenL, trainSize)
+            self.w, self.b = self.__update_wb(self.w, self.b, dw, db, alpha)
+            if (i+1) % 50 == 0:
+                print("Iteration: ", (i+1))
+                print("Accuracy: ", self.get_accuracy(self.get_predictions(a, self.numHiddenL), Y_train, trainSize))
+        return
+    
+
+    # Public method for testing the net with a given set of data. Prints the accuracy and returns
+    # the final output layer
+    def test(self, A0_test, Y_test, testSize):
+        y = self.__makeYUsable(Y_test)
+        a, _ = self.__forward_prop(A0_test, self.w, self.b, self.numHiddenL)
+        print("Accuracy: ", self.get_accuracy(self.get_predictions(a, self.numHiddenL), Y_test, testSize))
+        return a[self.numHiddenL + 1]
 
--- Number-Recognizer.py
+++ Number-Recognizer.py
@@ -12,11 +12,26 @@ Y_train = trainData[0] # Desired Result
 A0_train = trainData[1:trainDim]
 A0_train = A0_train / 255.0 # Normalize values between 0 and 1
 
+# Import the testing data
+testData = pd.read_csv('mnist_test.csv')
+testData = np.array(testData)
+testSize, testDim = testData.shape
+testData = testData.T
+Y_test = testData[0] # Desired Result
+A0_test = testData[1:testDim]
+A0_test = A0_test / 255.0 # Normalize values between 0 and 1
+
 
 # Neural Net Architecture
 inputLayerSize = trainDim - 1 # The first col is the desired result so not part of the input size
 outputLayerSize = 10
 hiddenLayerSizes = [16, 16]
+iterations = 50
+alpha = 0.5
 
+# Creating the network with random values
 neuralNet = NNet.__init__(inputLayerSize, outputLayerSize, hiddenLayerSizes)
+
+# Training
+neuralNet.train(A0_train, Y_train, trainSize, iterations, alpha)
 
