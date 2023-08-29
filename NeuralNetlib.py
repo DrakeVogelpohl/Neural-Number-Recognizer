Fixed Softmax divide by 0 issue

NeuralNetlib.py
@@ -42,7 +42,7 @@ class NeuralNet:
         return da * tanhDeriv
 
     def __Softmax(self, Z):
-        exp = np.exp(Z - Z.max())
+        exp = np.exp(Z - np.max(Z, axis=0))
         return exp / np.sum(exp, axis=0)
     
     def __SoftmaxDerivative(self, Z, da): 

Number-Recognizer.py
@@ -12,7 +12,6 @@ Y_train = trainData[0] # Desired Result
 A0_train = trainData[1:trainDim]
 A0_train = A0_train / 255.0 # Normalize values between 0 and 1
 
-
 # Import the testing data
 testData = pd.read_csv('mnist_test.csv')
 testData = np.array(testData)
@@ -27,21 +26,25 @@ A0_test = A0_test / 255.0 # Normalize values between 0 and 1
 inputLayerSize = trainDim - 1 # The first col is the desired result so not part of the input size
 outputLayerSize = 10
 hiddenLayerSizes = [100, 10]
-iterations = 10000
-stochasticGD = 1
-alpha = 1
-print("Alpha: ", alpha)
 
 # Creating the network with random values
 neuralNet = NNet(inputLayerSize, outputLayerSize, hiddenLayerSizes)
 
+
+# Training Architecture
 # Supported activation functions: "Sigmoid", "Tanh", "Softmax", "ReLU", "LeakyReLU", "ELU"
+iterations = 10000
+stochasticGD = 1
+alpha = 0.7
 activationFunction = "LeakyReLU"
 outActFunc = "Softmax"
+print("Alpha: ", alpha)
 
 # Training
 print("\nTraining")
-neuralNet.train(activationFunction, outActFunc, A0_train, Y_train, trainSize, iterations, alpha, dispFreq=2500, SGD=stochasticGD, batchSize=100)
+neuralNet.train(activationFunction, outActFunc, A0_train, Y_train, trainSize, iterations, alpha, 
+                dispFreq=5000, SGD=stochasticGD, batchSize=50)
+
 
 # Testing
 print("\nTesting")